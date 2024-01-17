f06a1aeeba0dd3e071e34724316840ecc49facf3
l <l>
2024-01-17 17:04:32 -0500

    planteamiento del ejercicio

---
+++ ej-clase.py
@@ -0,0 +1,53 @@
+def infoEquipo(nombre, goles):
+
+
+
+def main():
+    print('INFORMACIÓN DE LOS PARTIDOS')
+    for i in range(2):
+        # informacion para cada equipo
+        equipo = input('Equipo: ')
+        goles = int(input('Goles: '))
+        equipo_{i} = 'nombre'
+
+    equipo_1 = {
+        equipo: 'xxx',
+        pj: 0, # # partidos jugados
+        pg: 0, # # partidos ganados
+        pp: 0, # # partidos perdidos
+        pe: 0, # # partidos empatados
+        gf: 0, # # Goles a favor
+        gc: 0, # # Goles en contra
+        tp: 0, # Total puntos
+    }
+
+    
+
+
+
+
+
+if __name__ == '__main__':
+    main()
+
+
+    # Pedir información
+    """"
+        REGISTRO DE PARTIDOS
+        -----------------------
+        PERTIDO 1
+        -----------------------
+        Equipo local : xxxx
+        Goles: xxxx
+        Equipo Visistante: xxxx
+        Goles: xxxx
+    """
+
+    # Salida información
+    """
+    Equipo ganador xxx
+    Equipo perdedor xxx
+    """
+
+    # Para los reportes
+    # A
