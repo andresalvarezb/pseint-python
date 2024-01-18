c768f9f453aba822785c3e9f822f13c36f3332e8
l <l>
2024-01-18 15:58:53 -0500

    Eliminación de codigo extra y avances en la manera de registrar un partido

--- ej-clase.py
+++ ej-clase.py
@@ -7,94 +7,97 @@
 #         gf: 0, # # Goles a favor
 #         gc: 0, # # Goles en contra
 #         tp: 0, # Total puntos
-
-
-# def registroPartidos():
-#     partidosJugados = []
-
-#     print('REGISTRO DE PARTIDOS')
-#     partidoNumero = 1
-
-#     condicion = True
-#     while condicion:
-#         # variables locales
-#         ganador = False
-
-#         print(f"""
-#         {'-'*10}
-#         PARIDO {partidoNumero}
-#         {'-'*10}""")
-
-#         # registro de partidos
-#         local = input('Equipo local: ').capitalize()
-#         gol_local = int(input('Goles: '))
-#         visitante = input('Equipo visitante: ').capitalize()
-#         gol_visitante = int(input('Goles: '))
-
-#         # Determinar quien es ganador
-#         if gol_local > gol_visitante:
-#             ganador = True
-#             partidosJugados.append([local, gol_local, ganador])
-#         else:
-#             partidosJugados.append([visitante, gol_visitante, ganador])
-
-#         opcion = int(input('Nota: Oprima 1 para agregar otro registo, de lo contrario cualquier tecla: '))
-#         if opcion != 1:
-#             condicion = False
-#         partidoNumero += 1
-
-#     return partidosJugados
-
 def main():
-    tablaPosiciones = []
+    # CREACION DE EQUIPOS
+    # LECTURA DE EQUIPOS (TABLA DE POSICIONES)
+    # ACTUALIZACIÓN DE EQUIPOS
+    # ELIMINACINACIÓN DE EQUIPOS (SALIDA DEL PROGRAMA)
+
+    tablaEquipos = []
 
 
     print("""
-        Menú
+    MENÚ
     1 - Registrar una fecha
-    2 - Consultar tabla de posiciones""")
+    2 - Consultar tabla de posiciones
+    3 - Salida del programa""")
 
     opcion = int(input('Opción: '))
+    
     if opcion == 1:
-        partidosJugados = []
+        # TODO: Por cada fecha, ingresar los partidos jugados
+        # * 1. ciclo para las diferentes fechas
+        hayFecha = True
+        fecha = 1
+        while hayFecha:
+            print(f'Fecha #{fecha}')
+            # * 2. Ciclo para el registro de los diferentes partidos
 
-    # REGISTRO DE PARTIDOS
-    print('REGISTRO DE PARTIDOS')
-    partidoNumero = 1
+            hayPartidos = True 
+            partido = 1
+            while hayPartidos:
+                print(f"""
+                {'-'*7}
+                PARIDO {partido}
+                {'-'*7}""")
 
-    condicion = True
-    while condicion:
-        ganador = False
+                # ! RESGISTRO DE PARTIDOS
+                # ? Optención de datos de un partido
+                equipos_en_partido = []
+                TIPOS_DE_EQUIPO = ['local', 'visitante']
+                for i in range(len(TIPOS_DE_EQUIPO)):
+                    equipo = input(f'Equipo {TIPOS_DE_EQUIPO[i]}: ').lower()
+                    goles = int(input('Goles: '))
+                    equipos_en_partido.append([equipo, goles])
+                
+                # TODO: Validar si alguno de los equipos ingresados ya existe en la tabla de posiciones
+                    
+                # ? Ingreso de los datos del partido a la tabla de equipos
+                # Optención de la lista de equipos actuales en la tabla de equipos
+                equiposActuales = []
+                for equipo in range(tablaEquipos):
+                    equiposActuales.append(tablaEquipos[equipo][0])
 
-        print(f"""
-        {'-'*10}
-        PARIDO {partidoNumero}
-        {'-'*10}""")
+                
+                # * 1. Agregar equipo a tabla en caso de no existir
+                # verificación de la existencia de un equipo en la tabla de equipos
+                for equipo in range(equiposActuales):
+                    for i in range(len(equipos_en_partido)):
+                        if  equipos_en_partido[i] in equiposActuales[equipo]:
+                            # actualice valores
+                            pass
+                        else:
+                            # agregue el equipo a la tabla con sus valores faltantes
+                    # actualizar tabla
+                    # lista de equipos actuales
+                    for j
+                    equiposActuales.append(tablaEquipos[equipo][0])
+                    
+                    if tablaEquipos[equipo][0]:
+                        pass
+                    else:
+                        # agregar a la tabla
+                        pass
+                # * 2. Actualizar valores del equipo en caso de existir
 
-        # registro de partidos
-        local = input('Equipo local: ').capitalize()
-        gol_local = int(input('Goles: '))
-        visitante = input('Equipo visitante: ').capitalize()
-        gol_visitante = int(input('Goles: '))
+                # continuacion del ciclo para el ingreso de partidos
+                otroPartido = input('Agregar otro partido? [Y/N]: ')
+                if otroPartido.lower() == 'y':
+                    otroPartido = True
+                else:
+                    otroPartido = False    
+            
+            # continuación del ciclo para el ingreso de fechas
+            otra_fecha = input('Agregar otra fecha? [Y/N]: ')
+            if otra_fecha.lower() == 'y':
+                hayFecha = True
+            else:
+                hayFecha = False
 
-        # Determinar quien es ganador
-        if gol_local > gol_visitante:
-            ganador = True
-            partidosJugados.append([local, gol_local, ganador])
-        partidosJugados.append([visitante, gol_visitante, ganador])
-
-        opcion = int(input('Nota: Oprima 1 para agregar otro registo, de lo contrario cualquier tecla: '))
-        if opcion != 1:
-            condicion = False
-        partidoNumero += 1
-
-        tablaPosiciones.append(partidosJugados)
-        # for i in range(len(partidoFecha)):
-        #     if i[0] in 
-        # extracción del nombre de los equipos
-        # for equipo in range(len(tablaPosiciones)):
-        #     equiposParticipantes.append(equipo[0])
-        print(tablaPosiciones)
+    elif opcion == 2:
+        pass
+    elif opcion == 3:
+        pass
     else:
         pass
 
@@ -103,20 +106,7 @@ def main():
 if __name__ == '__main__':
     main()
 
-#     'Estructura de las listas'
 
-
-#     # Pedir información
-#     """"
-#         REGISTRO DE PARTIDOS
-#         -----------------------
-#         PERTIDO 1
-#         -----------------------
-#         Equipo local : xxxx
-#         Goles: xxxx
-#         Equipo Visistante: xxxx
-#         Goles: xxxx
-#     """
 
 #     # Salida información
 #     """
