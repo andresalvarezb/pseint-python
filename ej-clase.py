ca8c4302816da1675f09aa71b19928da4a9875e3
Andres_Alvarez <andres.alvarez094@hotmail.es>
2024-01-17 21:35:49 -0500

    Eliminación del codigo repetido

--- ej-clase.py
+++ ej-clase.py
@@ -1,140 +1,98 @@
 
-"def main():
-    print('INFORMACIÓN DE LOS PARTIDOS')
-    for i in range(2):
-        # informacion para cada equipo
-        equipo = input('Equipo: ')
-        goles = int(input('Goles: '))
-        equipo_{i} = 'nombre'
-
-    equipo_1 = {
-        equipo: 'xxx',
-        pj: 0, # # partidos jugados
-        pg: 0, # # partidos ganados
-        pp: 0, # # partidos perdidos
-        pe: 0, # # partidos empatados
-        gf: 0, # # Goles a favor
-        gc: 0, # # Goles en contra
-        tp: 0, # Total puntos
-    }
+#         equipo: 'xxx',
+#         pj: 0, # # partidos jugados
+#         pg: 0, # # partidos ganados
+#         pp: 0, # # partidos perdidos
+#         pe: 0, # # partidos empatados
+#         gf: 0, # # Goles a favor
+#         gc: 0, # # Goles en contra
+#         tp: 0, # Total puntos
 
 
 def registroPartidos():
-    partidos = []
+    partidosJugados = []
 
     print('REGISTRO DE PARTIDOS')
-    partido = 1
-    while True:
+    partidoNumero = 1
+
+    condicion = True
+    while condicion:
+        # variables locales
+        ganador = False
+
         print(f"""
         {'-'*10}
-        PARIDO {partido += 1}
+        PARIDO {partidoNumero}
         {'-'*10}""")
+
+        # registro de partidos
         local = input('Equipo local: ').capitalize()
         gol_local = int(input('Goles: '))
         visitante = input('Equipo visitante: ').capitalize()
         gol_visitante = int(input('Goles: '))
-        
+
         # Determinar quien es ganador
         if gol_local > gol_visitante:
             ganador = True
-            
-        partidos.append([local, gol_local])
-        partidos.append([visitante, gol_visitante])
+            partidosJugados.append([local, gol_local, ganador=False])
+        else:
+            partidosJugados.append([visitante, gol_visitante, ganador])
+
         opcion = int(input('Nota: Oprima 1 para agregar otro registo, de lo contrario cualquier tecla: '))
         if opcion != 1:
-            return False
-    
-    
-def tablaPosiciones():
-    pass
+            condicion = False
+        partidoNumero += 1
+
+    return partidosJugados
 
 def main():
-    print("""Menú
+    tablaPosiciones = []
+    equiposParticipantes = []
+
+
+    print("""
+        Menú
     1 - Registrar una fecha
     2 - Consultar tabla de posiciones""")
-    
+
     opcion = int(input('Opción: '))
     if opcion == 1:
-        registroPartidos()
+        partidoFecha = registroPartidos()
+        tablaPosiciones.append(partidoFecha)
+        # for i in range(len(partidoFecha)):
+        #     if i[0] in 
+        # extracción del nombre de los equipos
+        # for equipo in range(len(tablaPosiciones)):
+        #     equiposParticipantes.append(equipo[0])
+        print(tablaPosiciones)
     else:
-        tablaPosiciones()
+        pass
 
-
-if __name__ == '__main__':
-    main()
-
-    'Estructura de las listas'
-    # [equipo, pj, pg, pp, pe, gf, gc, tp]
-
-    # Pedir información
-    """"
-        REGISTRO DE PARTIDOS
-        -----------------------
-        PERTIDO 1
-        -----------------------
-        Equipo local : xxxx
-        Goles: xxxx
-        Equipo Visistante: xxxx
-        Goles: xxxx
-    """
-
-    # Salida información
-    """
-    Equipo ganador xxx
-    Equipo perdedor xxx
-    """
-
-    # Para los reportes
-    # Adef infoEquipo(nombre, goles):
-
-
-
-def main():
-    print('INFORMACIÓN DE LOS PARTIDOS')
-    for i in range(2):
-        # informacion para cada equipo
-        equipo = input('Equipo: ')
-        goles = int(input('Goles: '))
-        equipo_{i} = 'nombre'
-
-    equipo_1 = {
-        equipo: 'xxx',
-        pj: 0, # # partidos jugados
-        pg: 0, # # partidos ganados
-        pp: 0, # # partidos perdidos
-        pe: 0, # # partidos empatados
-        gf: 0, # # Goles a favor
-        gc: 0, # # Goles en contra
-        tp: 0, # Total puntos
-    }
-
-    
-
-
-
-
+#     # [equipo, pj, pg, pp, pe, gf, gc, tp]
 
 if __name__ == '__main__':
     main()
 
+#     'Estructura de las listas'
 
-    # Pedir información
-    """"
-        REGISTRO DE PARTIDOS
-        -----------------------
-        PERTIDO 1
-        -----------------------
-        Equipo local : xxxx
-        Goles: xxxx
-        Equipo Visistante: xxxx
-        Goles: xxxx
-    """
 
-    # Salida información
-    """
-    Equipo ganador xxx
-    Equipo perdedor xxx
-    """
+#     # Pedir información
+#     """"
+#         REGISTRO DE PARTIDOS
+#         -----------------------
+#         PERTIDO 1
+#         -----------------------
+#         Equipo local : xxxx
+#         Goles: xxxx
+#         Equipo Visistante: xxxx
+#         Goles: xxxx
+#     """
 
-    # Para los reportes
-    # A
+#     # Salida información
+#     """
+#     Equipo ganador xxx
+#     Equipo perdedor xxx
+#     """
+
+#     # Para los reportes
+#     # A
