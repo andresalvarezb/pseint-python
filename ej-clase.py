8d13c5f4e79a2fab095eb2538a76cb5be8134fd0
Andres_Alvarez <andres.alvarez094@hotmail.es>
2024-01-17 21:45:41 -0500

    Se pasa el codigo del registro de partidos a la función principal. main

--- ej-clase.py
+++ ej-clase.py
@@ -9,15 +9,61 @@
 #         tp: 0, # Total puntos
 
 
-def registroPartidos():
-    partidosJugados = []
+# def registroPartidos():
+#     partidosJugados = []
 
+#     print('REGISTRO DE PARTIDOS')
+#     partidoNumero = 1
+
+#     condicion = True
+#     while condicion:
+#         # variables locales
+#         ganador = False
+
+#         print(f"""
+#         {'-'*10}
+#         PARIDO {partidoNumero}
+#         {'-'*10}""")
+
+#         # registro de partidos
+#         local = input('Equipo local: ').capitalize()
+#         gol_local = int(input('Goles: '))
+#         visitante = input('Equipo visitante: ').capitalize()
+#         gol_visitante = int(input('Goles: '))
+
+#         # Determinar quien es ganador
+#         if gol_local > gol_visitante:
+#             ganador = True
+#             partidosJugados.append([local, gol_local, ganador])
+#         else:
+#             partidosJugados.append([visitante, gol_visitante, ganador])
+
+#         opcion = int(input('Nota: Oprima 1 para agregar otro registo, de lo contrario cualquier tecla: '))
+#         if opcion != 1:
+#             condicion = False
+#         partidoNumero += 1
+
+#     return partidosJugados
+
+def main():
+    tablaPosiciones = []
+
+
+    print("""
+        Menú
+    1 - Registrar una fecha
+    2 - Consultar tabla de posiciones""")
+
+    opcion = int(input('Opción: '))
+    if opcion == 1:
+        partidosJugados = []
+
+    # REGISTRO DE PARTIDOS
     print('REGISTRO DE PARTIDOS')
     partidoNumero = 1
 
     condicion = True
     while condicion:
-        # variables locales
         ganador = False
 
         print(f"""
@@ -34,31 +80,15 @@ def registroPartidos():
         # Determinar quien es ganador
         if gol_local > gol_visitante:
             ganador = True
-            partidosJugados.append([local, gol_local, ganador=False])
-        else:
-            partidosJugados.append([visitante, gol_visitante, ganador])
+            partidosJugados.append([local, gol_local, ganador])
+        partidosJugados.append([visitante, gol_visitante, ganador])
 
         opcion = int(input('Nota: Oprima 1 para agregar otro registo, de lo contrario cualquier tecla: '))
         if opcion != 1:
             condicion = False
         partidoNumero += 1
 
-    return partidosJugados
-
-def main():
-    tablaPosiciones = []
-    equiposParticipantes = []
-
-
-    print("""
-        Menú
-    1 - Registrar una fecha
-    2 - Consultar tabla de posiciones""")
-
-    opcion = int(input('Opción: '))
-    if opcion == 1:
-        partidoFecha = registroPartidos()
-        tablaPosiciones.append(partidoFecha)
+        tablaPosiciones.append(partidosJugados)
         # for i in range(len(partidoFecha)):
         #     if i[0] in 
         # extracción del nombre de los equipos
